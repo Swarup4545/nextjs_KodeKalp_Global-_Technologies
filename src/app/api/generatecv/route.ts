6f9a80661d7936b771086e013840d6e319df1c97
swarupkkk <swarupkkashid.tekhnologia@gmail.com>
2024-09-20 21:25:10 +0530

    first commit

--- src/app/api/generatecv/route.ts
+++ src/app/api/generatecv/route.ts
@@ -2,7 +2,7 @@ import { connect } from '@/dbConfig/dbConfig';
 import Resume from '@/models/resumeModel';
 import { NextRequest, NextResponse } from '../../../../node_modules/next/server';
 
-export async function GET(req:NextRequest) {
+export async function GET(req: NextRequest) {
   await connect();
   const { searchParams } = new URL(req.url);
   const email = searchParams.get('email');
@@ -13,7 +13,13 @@ export async function GET(req:NextRequest) {
       return NextResponse.json({ message: 'User not found' }, { status: 404 });
     }
     return NextResponse.json({ user }, { status: 200 });
-  } catch (error:unknown) {
-    return NextResponse.json({ message: 'Server error', error: error.message }, { status: 500 });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      // Safely access error.message only if it's an instance of Error
+      return NextResponse.json({ message: 'Server error', error: error.message }, { status: 500 });
+    } else {
+      // Fallback for other types of errors
+      return NextResponse.json({ message: 'Unknown server error' }, { status: 500 });
+    }
   }
 }
